25a53da3b87ed9b68fe1491fc380ece1a04659dc
Will Vauclain <willvauclain.dev@gmail.com>
2021-07-01 23:29:46 -0400

    Do stage0
    I am not super happy with the current layout. If I need to start
    implementing auxiliary files, I might have to rethink this moving
    forwards. Maybe I need one crate that produces multiple binaries, or I
    will just make one lib crate that everything else can import.

---
+++ impls/rust.2/step0_repl/src/main.rs
@@ -0,0 +1,46 @@
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+fn main() {
+    // `()` can be used when no completer is required
+    let mut rl = Editor::<()>::new();
+    if rl.load_history("history.txt").is_err() {
+        // println!("No previous history.");
+    }
+    loop {
+        let readline = rl.readline("user> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+
+                let output = print(eval(read(line)));
+                println!("{}", output);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    rl.save_history("history.txt").unwrap();
+}
+
+fn read(input: String) -> String {
+    input
+}
+
+fn eval(input: String) -> String {
+    input
+}
+
+fn print(input: String) -> String {
+    input
+}
